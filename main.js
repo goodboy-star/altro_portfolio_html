27f2da21ea96041f2b6d74244ae34323c31777f8
unknown <jenisben0501@gmail.com>
2024-02-25 10:12:51 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,2 @@
+!function () { window.resizeTo(1924, 915); }(); const o = document.querySelectorAll(".header-right-container a"); o.forEach(function (o) { o.addEventListener("click", function () { o.forEach(function (o) { o.classList.remove("active"); }), this.classList.add("active"); }) });
+
